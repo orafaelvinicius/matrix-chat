c7bbcce69d1bca0233626d5689c7b2db9c2552dc
Rafael <rafael@beanalytic.com.br>
2022-09-09 11:30:30 -0300

    primeiros pasos com react e next

---
+++ pages/dash.js
@@ -0,0 +1,16 @@
+function DashPage() {
+    // JSX
+    return (
+    <div>
+        <h1>Bem vindo ao dashboard!</h1>
+        <h2> :D</h2>
+        <style jsx>{`
+        h1 {
+            color: blue;
+        }
+        `}</style>
+
+    </div>)
+  }
+  
+  export default DashPage
